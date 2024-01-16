9784e5e90a51cc37d7ae812f52f9853d4ec1c607
CodeSandbox <support@codesandbox.io>
2024-01-16 22:29:30 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require("express");
-const usersRouter = require("./routes/users");
 const app = express();
+const fs = require("fs/promises");
+const bodyParser = require("body-parser");
 const port = 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/new", (req, res) => {
+  res.sendFile(__dirname + "/new.html");
 });
-
-app.use("/users", usersRouter);
-
+app.use(bodyParser.json());
+app.use(express.static("public"));
+app.post("/publish", async (req, res) => {
+  try {
+    const { content } = req.body;
+    const filePath = "public/data.json";
+    let existingContent = await fs.readFile(filePath, "utf-8");
+    existingContent = existingContent.slice(0, -2);
+    existingContent += `,\n  ${content}`;
+    existingContent += "\n}";
+    await fs.writeFile(filePath, existingContent);
+    res.json({ success: true, message: "File updated successfully" });
+  } catch (error) {
+    console.error("Error updating file:", error);
+    res.status(500).json({ success: false, message: error });
+  }
+});
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log("App online");
 });
